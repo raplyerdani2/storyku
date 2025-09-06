bd5315d8b47ce5dbccc240d59932d4c21668dcb9
Raply Erdani <raflyerdani2@gmail.com>
2025-09-07 01:34:46 +0700

    a

---
+++ src/view/page/favoritePage.js
@@ -0,0 +1,19 @@
+export const favroitePage = () => {
+  console.log(allStories);
+  root.innerHTML = `
+    <div id="storiesContainer">
+      <div id="mapStories"></div>
+      <h2 style="padding: 30px 0px 10px; text-align:center">Stories</h2>
+        <div id="storiesContainer2">
+          <div id="toContent" class="storiesContainerCard">
+            
+          </div>
+        </div>
+      </div>
+    `;
+
+  const header = document.getElementById("header");
+  const footer = document.getElementById("footer");
+  header.style.display = "flex";
+  footer.style.display = "flex";
+}
