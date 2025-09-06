31bd65b4f53dca359c2c3c9ef68a4223daec5f75
Raply Erdani <raflyerdani2@gmail.com>
2025-09-07 01:57:11 +0700

    a

--- src/view/page/storiesPage.js
+++ src/view/page/storiesPage.js
@@ -1,6 +1,15 @@
 import { getAllStoriesIdb } from "../../utils/db.js";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix ikon marker agar muncul di Webpack/Vite
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export const storiesPage = async (root, stories) => {
   let allStories = stories;
