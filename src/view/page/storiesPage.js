6bcb1c8a4e9c1939a5a9df40ac1f3e19390b32c6
Raply Erdani <raflyerdani2@gmail.com>
2025-09-07 02:00:38 +0700

    a

--- src/view/page/storiesPage.js
+++ src/view/page/storiesPage.js
@@ -3,10 +3,11 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 
-// Fix ikon marker agar muncul di Webpack/Vite
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
 });
